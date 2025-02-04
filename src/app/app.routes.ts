512f6d32084ba94c05cfc3f9da94a7ee5be99fbf
Durann23 <jd43573@gmail.com>
2025-02-03 19:26:47 -0600

    cambios para mejorar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
+import { title } from 'node:process';
 
 export const routes: Routes = [
     {
-        path: '', title: 'Home', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent), children: [
+        path: '', title: 'Como dice el dicho', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent), children: [
             { path: '', loadComponent: () => import('./principal/principal.component').then(m => m.PrincipalComponent) },
-            { path: 'contacto', loadComponent: () => import('./contacto/contacto.component').then(m => m.ContactoComponent) },
+            { path: 'contacto', data:{title:'Contacto', description:'wuu'},  loadComponent: () => import('./contacto/contacto.component').then(m => m.ContactoComponent) },
             { path: 'nosotros', loadComponent: () => import('./nosotros/nosotros.component').then(m => m.NosotrosComponent) },
             { path: 'catalogo', loadComponent: () => import('./catalogo/catalogo.component').then(m => m.CatalogoComponent) },
         ]
